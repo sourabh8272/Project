71bd30a255e7368943209cfe57d5a7c4438d1310
SOURABH MISHRA <91362600+sourabh8272@users.noreply.github.com>
2023-01-25 21:48:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>E - Voting System - Home</title>
+        <link rel="stylesheet" href="css/stylesheet.css">
+    </head>
+    <body>
+        
+            <center>
+            <div id="headerSection">
+            <h1>E - Voting System</h1>  
+            </div>
+            <hr>
+
+            <html>
+            <head>
+                <title>Background Images</title>
+            </head>
+            <body background="vote.jpg">
+                <center>
+                    <br>
+
+
+            <div id="loginSection">
+                <h2>Login</h2>
+                <form action="api/login.php" method="POST">
+                    <input type="number" name="mob" placeholder="Enter mobile" required><br><br>
+                    <input type="password" name="pass" placeholder="Enter password" required><br><br>
+                    <select name="role" style="width: 15%; border:olid black 2px s">
+                        <option value="1">Voter</option>
+                        <option value="2">Party</option>
+                    </select><br><br>                  
+                    <button id="loginbtn" type="submit" name="loginbtn">Login</button><br><br>
+                    New user? <a href="routes/register.php">Register here</a>
+                </form>
+            </div>
+
+            </center> 
+    </body>
+</html>
